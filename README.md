f9cf33c68bb8aec598faa4aba503ae50647751ae
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2024-02-08 23:47:27 +0000

    use lax.scan

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ def target_log_pdf(params):
 # run HMC
 params_init = jnp.zeros(10)
 key = jax.random.PRNGKey(0)
-chain = hmc.sample(params_init, target_log_pdf, n_steps=10, n_leapfrog_steps=100, step_size=0.1, key=key)
+chain = hmc.sample(key, params_init, target_log_pdf, n_steps=10, n_leapfrog_steps=100, step_size=0.1)
 ```
 
 Based on the [official repository](https://github.com/google-research/google-research/tree/master/bnn_hmc) for the paper [_What Are Bayesian Neural Network Posteriors Really Like?_](https://arxiv.org/abs/2104.14421)
--- hmc.py
+++ hmc.py
@@ -1,5 +1,23 @@
+import operator as op
 import jax
 import jax.numpy as jnp
+from jax.tree_util import tree_map, tree_leaves, tree_reduce
+
+
+def random_split_like_tree(rng_key, target=None, treedef=None):
+    if treedef is None: treedef = jax.tree_util.tree_structure(target)
+    keys = jax.random.split(rng_key, treedef.num_leaves)
+    return jax.tree_util.tree_unflatten(treedef, keys)
+
+
+def normal_like_tree(rng_key, target, mean=0, std=1):
+    # https://github.com/google/jax/discussions/9508#discussioncomment-2144076
+    keys_tree = random_split_like_tree(rng_key, target)
+    return tree_map(lambda l, k: mean + std*jax.random.normal(k, l.shape, l.dtype), target, keys_tree)
+
+
+def ifelse(cond, val_true, val_false):
+    return jax.lax.cond(cond, lambda x: x[0], lambda x: x[1], (val_true, val_false))
 
 
 def leapfrog(params, momentum, log_prob_fn, step_size, n_steps):
@@ -8,18 +26,18 @@ def leapfrog(params, momentum, log_prob_fn, step_size, n_steps):
     # define a single step
     def step(i, args):
         params, momentum = args
-        
+
         # update momentum
         grad = jax.grad(log_prob_fn)(params)
-        momentum += 0.5 * step_size * grad
+        momentum = tree_map(lambda m, g: m + 0.5 * step_size * g, momentum, grad)
 
         # update params
-        params += momentum * step_size
+        params = tree_map(lambda p, m: p + m * step_size, params, momentum)
 
         # update momentum
         grad = jax.grad(log_prob_fn)(params)
-        momentum += 0.5 * step_size * grad
-        
+        momentum = tree_map(lambda m, g: m + 0.5 * step_size * g, momentum, grad)
+
         return params, momentum
 
     # do 'n_steps'
@@ -28,42 +46,39 @@ def leapfrog(params, momentum, log_prob_fn, step_size, n_steps):
     return new_params, new_momentum
 
 
-def sample(params, log_prob_fn, n_steps, n_leapfrog_steps, step_size, key):
+def sample(key, params_init, log_prob_fn, n_steps, n_leapfrog_steps, step_size):
     """
-    Runs HMC and returns the full Markov chain as a list.
+    Runs HMC and returns the full Markov chain as a Pytree.
     - params: array
     - log_prob_fn: function that takes params as the only argument and returns a scalar value
     """
 
     # define a single step
-    def step(i, args):
-        params, chain, total_accept_prob, key = args
+    def step_fn(carry, x):
+        params, key = carry
         key, normal_key, uniform_key = jax.random.split(key, 3)
 
         # generate random momentum
-        momentum = jax.random.normal(normal_key, shape=params.shape)
+        momentum = normal_like_tree(key, params)
 
         # leapfrog
         new_params, new_momentum = leapfrog(params, momentum, log_prob_fn, step_size, n_leapfrog_steps)
 
         # MH correction
         potentaial_energy_diff = log_prob_fn(new_params) - log_prob_fn(params)
-        kinetic_energy_diff = 0.5*(momentum**2 - new_momentum**2).sum()
+        momentum_dot = tree_reduce(op.add, tree_map(lambda x: (x**2).sum(), tree_leaves(momentum)))
+        new_momentum_dot = tree_reduce(op.add, tree_map(lambda x: (x**2).sum(), tree_leaves(new_momentum)))
+        kinetic_energy_diff = 0.5*(momentum_dot - new_momentum_dot)
         log_accept_prob = potentaial_energy_diff + kinetic_energy_diff
         log_accept_prob = jnp.nan_to_num(log_accept_prob, nan=-jnp.inf)
         accept_prob = jnp.minimum(1, jnp.exp(log_accept_prob))
-        total_accept_prob += accept_prob
         accept = jax.random.uniform(uniform_key) < accept_prob
-        params = jnp.where(accept, new_params, params)
-        
-        # store params
-        chain = chain.at[i].set(params)
-         
-        return params, chain, total_accept_prob, key
-    
+        params = ifelse(accept, new_params, params)
+
+        return (params, key), (params, accept_prob)
+
     # do 'n_steps'
-    chain = jnp.zeros([n_steps, len(params)])
-    _, chain, total_accept_prob, key = jax.lax.fori_loop(0, n_steps, step, (params, chain, 0, key))
+    _, (chain, accept_prob) = jax.lax.scan(step_fn, (params_init, key), xs=None, length=n_steps)
     
-    print(f'Avg. accept. prob.: {(total_accept_prob/n_steps):.2%}')
+    print(f'accept={accept_prob.mean():.2%}')
     return chain
